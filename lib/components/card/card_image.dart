3526d29dcbeaf3975784bf8f2fe3e20c091086d7
Lucas Barbero <lucas95_273@hotmail.com>
2022-12-27 10:42:32 -0300

    Creacion del boton Favoritos

--- lib/components/card/card_image.dart
+++ lib/components/card/card_image.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import '../buttons/floating_action_button_green.dart';
 
 class CardImage extends StatelessWidget {
   String pathImage;
@@ -7,21 +8,27 @@ class CardImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 350.0,
-      width: 250.0,
-      margin: const EdgeInsets.only(top: 80.0, left: 20.0),
-      decoration: BoxDecoration(
-          image:
-              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage)),
-          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-          shape: BoxShape.rectangle,
-          boxShadow: const <BoxShadow>[
-            BoxShadow(
-                color: Colors.black38,
-                blurRadius: 15.0,
-                offset: Offset(0.0, 7.0))
-          ]),
+    return Stack(
+      alignment: const Alignment(0.9, 1.1),
+      children: <Widget>[
+        Container(
+          height: 350.0,
+          width: 250.0,
+          margin: const EdgeInsets.only(top: 80.0, left: 20.0),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover, image: AssetImage(pathImage)),
+              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              shape: BoxShape.rectangle,
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                    color: Colors.black38,
+                    blurRadius: 15.0,
+                    offset: Offset(0.0, 7.0))
+              ]),
+        ),
+        FloatingActionButtonGreen(),
+      ],
     );
   }
 }
